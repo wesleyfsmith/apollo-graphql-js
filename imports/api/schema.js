36ab552eb115336c7314150e4d4ff1de05ae0df2
wesley <wesleyfsmith@gmail.com>
2016-09-01 21:51:38 -0600

    Added graphql-js library integration and built native js schema

--- imports/api/schema.js
+++ imports/api/schema.js
@@ -1,5 +1,26 @@
 import { Random } from 'meteor/random';
 
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql';
+
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
+    }
+  })
+});
+
 export const schema = [`
 type Email {
   address: String

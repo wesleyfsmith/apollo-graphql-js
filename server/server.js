6d8a7fc8f703045452265142f426c391cbbf09ff
wesley <wesleyfsmith@gmail.com>
2016-09-08 11:47:39 -0600

    Added server initialization and npm modules

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import { createApolloServer } from 'meteor/apollo';
+
+import { schema, resolvers } from '/imports/api/schema';
+
+createApolloServer({
+    graphiql: true,
+    pretty: true,
+    schema,
+    resolvers,
+});
